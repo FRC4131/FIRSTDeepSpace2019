54673cf36b7d1d29e3a0047ef5cc8cb4a484385b
Andrew <chappelleandy@aol.com>
2019-01-14 19:31:44 -0800

    Drive PID Start

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,13 +1,10 @@
 package frc.robot;
 
 import com.kauailabs.navx.frc.AHRS;
-import edu.wpi.cscore.UsbCamera;
-import edu.wpi.cscore.VideoMode;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.cameraserver.CameraServer;
 
 
 public class Robot extends TimedRobot implements PIDOutput {
@@ -15,30 +12,53 @@ public class Robot extends TimedRobot implements PIDOutput {
 	Hand RightHand = GenericHID.Hand.kRight;
 	XboxController Controller = new XboxController(0);
 	AHRS ahrs = new AHRS(SPI.Port.kMXP);
-	static double kP = 0.0135;
-	static double kI = 0.00;
-	static double kD = 0.1;
-	static double kF = 0.00;
-	static double kToleranceDegrees = 2.0f;
+
 	PIDController turnController;
+	final double kPTurn = 0.0135;
+	final double kITurn = 0.00;
+	final double kDTurn = 0.1;
+	final double kToleranceDegrees = 2.0f;
+
+    PIDController driveController;
+    final double kPDrive = 0.00;
+    final double kIDrive = 0.00;
+    final double kDDrive = 0.00;
+    final double kToleranceDistance = 5;
+
+    private boolean hadDoublePOV = false; // TODO: rename because a bit misleading
+    private boolean isFieldCentric = true;
 
 	// Talons:
 	Talon FrontRight = new Talon(3);
 	Talon BackRight = new Talon(2);
 	Talon FrontLeft = new Talon(1);
 	Talon BackLeft = new Talon(0);
-	double rotateToAngleRate;
 
+	double rotateToAngleRate;
+    double driveToRate;
+    DrivePID drivePID = new DrivePID();
 	// Drive Base
 	MecanumDrive myDrive = new MecanumDrive(BackRight, FrontRight, BackLeft, FrontLeft);
 
 	public void robotInit() {
-        turnController = new PIDController(kP, kI, kD, kF, ahrs, this, .01);
+        turnController = new PIDController(kPTurn, kITurn, kDTurn, ahrs, this);
         turnController.setInputRange(-180.0f, 180.0f);
         turnController.setOutputRange(-1, 1);
-        myDrive.setDeadband(.1);
         turnController.setContinuous(true);
         turnController.setAbsoluteTolerance(kToleranceDegrees);
+        driveController = new PIDController(kPDrive, kIDrive, kDDrive, ahrs, drivePID);//TODO:fix the input source;
+        driveController.setOutputRange(-1,1);
+        driveController.setContinuous(false);
+        driveController.setAbsoluteTolerance(kToleranceDistance);
+        myDrive.setDeadband(.1);
+    }
+
+    public void autonomousInit(){
+	    turnController.enable();
+    }
+
+    public void autonomousPeriodic(){
+
     }
 
     public void teleopInit(){
@@ -48,18 +68,31 @@ public class Robot extends TimedRobot implements PIDOutput {
 	public void teleopPeriodic() {
         Reset();//Resets Gyro When you press A
 
-        SnapToAngle();//Turn to angle
+        centricToggle();
+
+        if (!ahrs.isConnected()) {
+            isFieldCentric = false;
+        }
 
         if (Controller.getTriggerAxis(LeftHand) > 0.05 || Controller.getTriggerAxis(RightHand) > 0.05) {
             strafe();
+            SnapToAngle();
+        } else if (isFieldCentric){
+            driveCentric();
+            SnapToAngle();
         } else {
-            DriveNormally();
+            driveStandard();
         }
 
+
+        SmartDashboard.putNumber("X Displacement", ahrs.getDisplacementX());
+        SmartDashboard.putNumber("Y Displacement", ahrs.getDisplacementY());
         SmartDashboard.putNumber("Target", turnController.getSetpoint());
         SmartDashboard.putNumber("Yaw", ahrs.getYaw());
+        SmartDashboard.putNumber("POV", Controller.getPOV());
+        SmartDashboard.putBoolean("hadDoublePOV", hadDoublePOV);
+        SmartDashboard.putBoolean("Field Centric Enabled", isFieldCentric);
 	}
-
 
 	@Override
 	public void pidWrite(double output) {
@@ -72,38 +105,64 @@ public class Robot extends TimedRobot implements PIDOutput {
         }
     }
 
-    public void SnapToAngle(){
-        if(Controller.getPOV()== 0) turnController.setSetpoint(0); //0
-        if(Controller.getPOV()== 45) turnController.setSetpoint(45);//90
-        if(Controller.getPOV()== 90) turnController.setSetpoint(90);//180
-        if(Controller.getPOV()== 135) turnController.setSetpoint(135);//-90
-        if(Controller.getPOV()== 180) turnController.setSetpoint(180);//0
-        if(Controller.getPOV()== 225) turnController.setSetpoint(-135);//90
-        if(Controller.getPOV()== 270) turnController.setSetpoint(-90);//180
-        if(Controller.getPOV()== 315) turnController.setSetpoint(-45);//-90
+    public void centricToggle() {
+	    if (Controller.getStartButton()) {
+	        isFieldCentric = !isFieldCentric;
+        } else {
+	        return;
+        }
+    }
 
+    public void SnapToAngle(){
+	    if (Controller.getPOV() == -1) { // no Controller POV pressed
+	        hadDoublePOV = false;
+	        return;
+        }
+
+        int controllerPOV = Controller.getPOV();
+        if (controllerPOV > 180) {
+            controllerPOV -= 360;
+        }
+
+        if (controllerPOV % 90 != 0) { // Controller POV is at mixed angle
+            hadDoublePOV = true;
+            turnController.setSetpoint(controllerPOV);
+        } else if (!hadDoublePOV) { // Controller POV only has one pressed AND no double POV pressed yet
+            turnController.setSetpoint(controllerPOV);
+        }
     }
 
     public void strafe(){
 	    if(Controller.getTriggerAxis(LeftHand) > 0.05 && Controller.getTriggerAxis(RightHand) > 0.05) {
 	        return;
         } else if (Controller.getTriggerAxis(LeftHand) > 0.05) {
-	        FrontLeft.set(-Controller.getTriggerAxis(LeftHand));
-	        BackLeft.set(Controller.getTriggerAxis(LeftHand));
-	        FrontRight.set(-Controller.getTriggerAxis(LeftHand));
-	        BackRight.set(Controller.getTriggerAxis(LeftHand));
+	        FrontLeft.set(Controller.getTriggerAxis(LeftHand));
+	        BackLeft.set(-Controller.getTriggerAxis(LeftHand));
+	        FrontRight.set(Controller.getTriggerAxis(LeftHand));
+	        BackRight.set(-Controller.getTriggerAxis(LeftHand));
         } else if (Controller.getTriggerAxis(RightHand) > 0.05) {
-            FrontLeft.set(Controller.getTriggerAxis(RightHand));
-            BackLeft.set(-Controller.getTriggerAxis(RightHand));
-            FrontRight.set(Controller.getTriggerAxis(RightHand));
-            BackRight.set(-Controller.getTriggerAxis(RightHand));
+            FrontLeft.set(-Controller.getTriggerAxis(RightHand));
+            BackLeft.set(Controller.getTriggerAxis(RightHand));
+            FrontRight.set(-Controller.getTriggerAxis(RightHand));
+            BackRight.set(Controller.getTriggerAxis(RightHand));
         } else {
 	        return;
         }
 	}
 
-    public void DriveNormally(){
+    public void driveCentric(){
         myDrive.driveCartesian(Controller.getX(LeftHand), -Controller.getY(LeftHand),
                 rotateToAngleRate, ahrs.getYaw()- 2 * turnController.getSetpoint());
+    }
+
+    public void driveStandard() {
+	    myDrive.driveCartesian(Controller.getX(LeftHand), -Controller.getY(LeftHand), Controller.getX(RightHand));
+	}
+
+	private class DrivePID implements PIDOutput {
+        @Override
+        public void pidWrite(double output) {
+            driveToRate = output;
+        }
     }
 }
