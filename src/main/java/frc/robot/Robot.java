4d716489570b8003ec89eea3955aee75110e9b12
Andrew <chappelleandy@aol.com>
2019-02-16 17:10:53 -0800

    More work as of 2/16

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,9 @@ import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
 import com.kauailabs.navx.frc.AHRS;
 
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.*;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
@@ -36,6 +39,9 @@ public class Robot extends TimedRobot implements PIDOutput {
     private static boolean armsUp = true;
     private static boolean intakeActive = false;
 
+    //the centers of the two retro-reflective targets, if both the x and y of a target are zero it isn't detected
+    private static double zeroCenterX, zeroCenterY, oneCenterX, oneCenterY;
+
     WPI_TalonSRX frontRight = new WPI_TalonSRX(3);
     WPI_TalonSRX backRight = new WPI_TalonSRX(1);
     WPI_TalonSRX frontLeft = new WPI_TalonSRX(4);
@@ -54,8 +60,10 @@ public class Robot extends TimedRobot implements PIDOutput {
     private static double rotateToAngleRate;
 
     MecanumDrive myDrive = new MecanumDrive(frontLeft, backLeft, frontRight, backRight);
-
     PIDController turnController = new PIDController(kPTurn, kITurn, kDTurn,kFTurn, ahrs, this);
+
+    AutoStrafer autoStrafer = new AutoStrafer();
+    PIDController strafeController = new PIDController(0.04, 0, 0, 0, autoStrafer, autoStrafer);
 
     public void robotInit() {
         elevator.setSelectedSensorPosition(0);
@@ -74,6 +82,13 @@ public class Robot extends TimedRobot implements PIDOutput {
         turnController.setContinuous(true);
         turnController.enable();
         turnController.setSetpoint(0);
+
+//        strafeController.setInputRange(-1000, 1000);
+        strafeController.setOutputRange(-1, 1);
+        strafeController.setAbsoluteTolerance(5);
+        strafeController.setContinuous(false);
+        strafeController.enable();
+        strafeController.setSetpoint(0);
     }
 
     public void autonomousInit(){
@@ -103,13 +118,20 @@ public class Robot extends TimedRobot implements PIDOutput {
     }
 
     public void run() {
+        ntReader();
         hatchMech();
         deployHatchMech();
         reset();
         spinArms();
         adjustElevator();
+        lifter.run();
         runIntake();
         centricToggle();
+
+        if (controller.getRawButton(5)) {
+            autoStrafer.run();
+            return;
+        }
 
         if (!ahrs.isConnected()) {
             isFieldCentric = false;
@@ -126,18 +148,38 @@ public class Robot extends TimedRobot implements PIDOutput {
         }
     }
 
+    private void ntReader() {
+        NetworkTableInstance inst = NetworkTableInstance.getDefault();
+        NetworkTable table = inst.getTable("vision");
+        NetworkTableEntry zeroX = table.getEntry("zeroX");
+        NetworkTableEntry zeroY = table.getEntry("zeroY");
+        NetworkTableEntry oneX = table.getEntry("oneX");
+        NetworkTableEntry oneY = table.getEntry("oneY");
+        inst.startServer();
+        inst.setServerTeam(4131);
+        this.zeroCenterX = zeroX.getDouble(0);
+        this.zeroCenterY = zeroY.getDouble(0);
+        this.oneCenterX = oneX.getDouble(0);
+        this.oneCenterY = oneY.getDouble(0);
+    }
+
     public void smartDash() {
         SmartDashboard.putBoolean("isCentric", isFieldCentric);
 
-        SmartDashboard.putNumber(   "Target",            turnController.getSetpoint());
-        SmartDashboard.putNumber(   "Set Point",            turnController.getSetpoint());
-        SmartDashboard.putBoolean(   "onTarget",            turnController.onTarget());
-        SmartDashboard.putNumber(   "Rotate to Angle Rate",        rotateToAngleRate);
-        SmartDashboard.putNumber(   "Get Angle",               ahrs.getAngle());
+        SmartDashboard.putNumber("Target", turnController.getSetpoint());
+        SmartDashboard.putNumber("Set Point", turnController.getSetpoint());
+        SmartDashboard.putBoolean("onTarget", turnController.onTarget());
+        SmartDashboard.putNumber("Rotate to Angle Rate", rotateToAngleRate);
+        SmartDashboard.putNumber("Get Angle", ahrs.getAngle());
 
         SmartDashboard.putBoolean("NavX Connected", ahrs.isConnected());
 
         SmartDashboard.putBoolean("Arms Up", armsUp);
+
+        SmartDashboard.putNumber("zeroX", zeroCenterX);
+        SmartDashboard.putNumber("zeroY", zeroCenterY);
+        SmartDashboard.putNumber("oneX", oneCenterX);
+        SmartDashboard.putNumber("oneY", oneCenterY);
     }
 
     @Override
@@ -158,8 +200,6 @@ public class Robot extends TimedRobot implements PIDOutput {
     public void centricToggle() {
         if (controller.getBackButtonReleased()) {
             isFieldCentric = !isFieldCentric;
-        } else {
-            return;
         }
     }
 
@@ -211,7 +251,8 @@ public class Robot extends TimedRobot implements PIDOutput {
     }
 
     public void driveCentric(){
-        myDrive.driveCartesian(-controller.getX(leftHand), controller.getY(leftHand), rotateToAngleRate, ahrs.getAngle() - 2*turnController.getSetpoint());
+//        myDrive.driveCartesian(-controller.getX(leftHand), controller.getY(leftHand), rotateToAngleRate, ahrs.getAngle() - 2*turnController.getSetpoint());
+        myDrive.driveCartesian(controller.getY(leftHand), controller.getX(leftHand), rotateToAngleRate, ahrs.getAngle());
     }
 
     private void adjustElevator(){
@@ -221,11 +262,7 @@ public class Robot extends TimedRobot implements PIDOutput {
             lifter.setTarget(11300);
         } else if(secondary.getRawButton(3)) {
             lifter.setTarget(0);
-        }  else {
-            return; // TODO: this looks like it should be removed...
         }
-
-        lifter.run();
     }
 
     private void runIntake() {
@@ -249,7 +286,6 @@ public class Robot extends TimedRobot implements PIDOutput {
              armsDeploy.set(DoubleSolenoid.Value.kForward);
          } else {
              armsDeploy.set(DoubleSolenoid.Value.kReverse);
-
          }
     }
 
@@ -263,5 +299,45 @@ public class Robot extends TimedRobot implements PIDOutput {
 
     public void driveStandard() {
         myDrive.driveCartesian(-controller.getX(leftHand), controller.getY(leftHand), controller.getX(rightHand));
+    }
+
+    public void strafeCentric(double val) { //TODO: test
+        myDrive.driveCartesian(val, 0, rotateToAngleRate, ahrs.getAngle() - 2*turnController.getSetpoint());
+    }
+
+    private class AutoStrafer implements PIDSource, PIDOutput {
+        PIDSourceType pidSourceType = PIDSourceType.kDisplacement;
+        double pidOut = 0;
+
+        public void run() {
+            strafeCentric(pidOut);
+        }
+
+        @Override
+        public void pidWrite(double output) {
+            pidOut = output;
+        }
+
+        @Override
+        public void setPIDSourceType(PIDSourceType pidSource) {
+            this.pidSourceType = pidSource;
+        }
+
+        @Override
+        public PIDSourceType getPIDSourceType() {
+            return pidSourceType;
+        }
+
+        @Override
+        public double pidGet() {
+            if (pidSourceType == PIDSourceType.kDisplacement) {
+                // assumes 0 is center of frame
+                return (zeroCenterX + oneCenterX) / 2;
+            } else {
+                // I don't really care about velocity for these... I think.
+                // TODO: If it doesn't work, may need to implement kVelocity
+                return 0;
+            }
+        }
     }
 }
